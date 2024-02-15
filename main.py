10df9c37cb2593cb91499878440f1f3beebf2fce
flawisss <artem_kurlov2007@mail.ru>
2024-02-15 21:03:57 +0500

    third task complete

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from flask import render_template, Flask
 app = Flask(__name__)
 
 
-@app.route('/training/<profession>')
-def training(profession):
-    return render_template(template_name_or_list='index.html', profession=profession)
+@app.route('/list_prof/<list>')
+def training(list):
+    professions = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач', 'инженер по терраформированию',
+                   'климатолог', 'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+                   'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер', 'штурман',
+                   'пилот дронов']
+    return render_template(template_name_or_list='index.html', list=list, professions=professions, lenth=len(professions))
 
 
 if __name__ == '__main__':
